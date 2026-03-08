1d8f2f0db9709c1a1e03d8d23061488a95018e4a
PriceLlewellyn <price13.llewellyn@gmail.com>
2026-03-08 15:26:02 +0000

    all function created

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const all = good + neutral + bad;
+
+  
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNuetralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  
+    const handlebadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNuetralClick} text='neutral' />
+      <Button onClick={handlebadClick} text='bad' />
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+    </div>
+  )
+}
+
+export default App
